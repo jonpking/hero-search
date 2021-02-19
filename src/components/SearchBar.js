76986228de344f37bc4dd373b424106a5bb7ee0e
Jonathan King <jonpking@gmail.com>
2021-02-18 20:33:36 -0700

    started adding SearchBar functionality

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,31 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+    state = {
+        searchTerm: ""
+    };
+
+    onSearchSubmit = (event) => {
+        event.preventDefault();
+    };
+
     render() {
         return (
-            <div className="ui form">
-                <div className="field">
-                    <label>Search Here</label>
-                    <input type="text" />
-                </div>
+            <div className="search-bar ui segment">
+                <form className="ui form" onSubmit={this.onSearchSubmit}>
+                    <div className="field">
+                        <label>Search Here</label>
+                        <input
+                            type="text"
+                            value={this.state.searchTerm}
+                            onChange={
+                                e => this.setState({
+                                    searchTerm: e.target.value
+                                })
+                            }
+                        />
+                    </div>
+                </form>
             </div>
         );
     }
