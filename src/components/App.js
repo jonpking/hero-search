6e11307e9783368eea5238dd50551339fecc49b5
Jonathan King <jonpking@gmail.com>
2021-02-21 11:17:22 -0700

    moved state and input handling from SearchBar to App

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,28 @@ import React from "react";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
+    state = {
+        searchTerm: ""
+    };
+
+    handleSearchSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state.searchTerm);
+    };
+
+    handleSearchChange = (event) => {
+        this.setState({ searchTerm: event.target.value });
+    };
+
     render() {
         return (
             <div className="ui container">
                 <div className="ui segment">
-                    <SearchBar />
+                    <SearchBar
+                        onSearchSubmit={this.handleSearchSubmit}
+                        searchTerm={this.state.searchTerm}
+                        onSearchChange={this.handleSearchChange}
+                    />
                 </div>
             </div>
         );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,34 +1,20 @@
 import React from "react";
 
-class SearchBar extends React.Component {
-    state = {
-        searchTerm: ""
-    };
-
-    onSearchSubmit = (event) => {
-        event.preventDefault();
-    };
-
-    render() {
-        return (
-            <div className="search-bar ui segment">
-                <form className="ui form" onSubmit={this.onSearchSubmit}>
-                    <div className="field">
-                        <label>Search Here</label>
-                        <input
-                            type="text"
-                            value={this.state.searchTerm}
-                            onChange={
-                                e => this.setState({
-                                    searchTerm: e.target.value
-                                })
-                            }
-                        />
-                    </div>
-                </form>
-            </div>
-        );
-    }
+const SearchBar = (props) => {
+    return (
+        <div className="search-bar ui segment">
+            <form className="ui form" onSubmit={props.onSearchSubmit}>
+                <div className="field">
+                    <label>Search Here</label>
+                    <input
+                        type="text"
+                        value={props.searchTerm}
+                        onChange={props.onSearchChange}
+                    />
+                </div>
+            </form>
+        </div>
+    )
 };
 
 export default SearchBar;
