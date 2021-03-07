968c92f261324f9e5a28a9fbb19258227b2f6a75
Jonathan King <jonpking@gmail.com>
2021-03-07 15:27:56 -0700

    passed data from search into App state and then into HeroList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import HeroList from "./HeroList";
 import API from "../apis/api";
 
 class App extends React.Component {
+    state = {
+        heroData: []
+    }
+
     handleSearchSubmit = async (searchTerm, event) => {
         event.preventDefault();
-        console.log(searchTerm);
         const response = await API.get(`search/${searchTerm}`);
-        console.log(response);
+        this.setState({ heroData: response.data.results });
+        console.log(this.state.heroData);
     };
 
     render() {
@@ -17,6 +22,9 @@ class App extends React.Component {
                     <SearchBar
                         onSearchSubmit={this.handleSearchSubmit}
                     />
+                </div>
+                <div className="ui segment">
+                    <HeroList heroData={this.state.heroData} />
                 </div>
             </div>
         );
